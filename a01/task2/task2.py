4fc789ffc768e9339d72e64b8b359344d28cb0b5
MJT2401 <66429517+sudo-whoami@users.noreply.github.com>
2024-10-21 20:45:24 +0200

    Update task2.py

--- a01/task2/task2.py
+++ a01/task2/task2.py
@@ -9,6 +9,8 @@ ax1 = fig1.add_subplot()
 ax1.bar(computer_dat[:,0], computer_dat[:,1])
 ax1.set_xlabel("Year")
 ax1.set_ylabel("Transistor Count")
+# We can use the log-scale for this plot, as the transistor count rises exponentially. This does not apply to the Computer perfomance.
+ax1.set_yscale("log")
 
 fig2 = plt.figure()
 ax2 = fig2.add_subplot()
