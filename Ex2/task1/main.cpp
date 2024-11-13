211443e56c5f0d9b70986a7e139671df9ad7a6b9
Bailiang Li <lbllbl93@gmail.com>
2024-11-14 00:14:20 +0100

    task1 basic done

--- Ex2/task1/main.cpp
+++ Ex2/task1/main.cpp
@@ -5,6 +5,8 @@
 #include <chrono>
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
 
 namespace timeit {
 
@@ -39,11 +41,23 @@ struct Test {
     std::printf("vector length: %zu\n", N);
     std::printf("memory footprint: %f kbytes\n",
                 N * 4.0 * sizeof(double) / 1024.0);
-    std::printf("total flops performed: %zu\n", N * 2);
+    std::printf("total flops performed: %zu\n", N * 2); // FLOPs per a=b+c*d
     std::printf("runtime per sweep: %e\n", runtime);
   }
+  void print1() const { std::printf("%zu %zu %e\n", N, 2*N, runtime); } // #iter, #FLOP, runtime
   void print2() const { std::printf("%zu %e\n", N, runtime); }
 };
+
+// split line by required deliminator
+std::vector<std::string> split (const std::string &s, char delim) {
+    std::vector<std::string> result;
+    std::stringstream ss(s);
+    std::string item;
+    while (getline(ss, item, delim)) {
+        if(item.length()!=0){ result.push_back(item); }
+    }
+    return result;
+}
 
 int main() {
   std::vector<Test> Tests = {
@@ -51,15 +65,60 @@ int main() {
       8'000,   20'000,  30'000,    40'000,    50'000,    60'000,    100'000,
       200'000, 300'000, 1'000'000, 2'000'000, 5'000'000, 10'000'000};
 
+  // 1: vector length (N) VS FLOPs
+  for (auto &test : Tests) {
+    Vector a(test.N, 1.0);  // 200/300/.../10000000 ones
+    Vector b(test.N, 1.0);  // 200/300/.../10000000 ones
+    Vector c(test.N, 1.0);  // 200/300/.../10000000 ones
+    Vector d(test.N, 1.0);  // 200/300/.../10000000 ones
+    // average triad runtime
+    for (int i=0; i!=40; ++i){
+      test.runtime+=timeit::run(triad, a, b, c, d);
+    }
+    test.runtime/=40;
+    // FLOPs/s
+    double FLOPs=2*test.N/test.runtime;
+    std::cout<<"FLOPs: "<<FLOPs<<std::endl;
+    // test.print1(); // details
+  }
+  
+  // 2: indicate cache size of my system
+  system("rm ./system_info.txt");
+  system("lscpu >> ./system_info.txt");
+  double L1d, L1i, L2, L3;
+  std::ifstream system_info;
+  system_info.open("./system_info.txt");
+  std::string value;
+  while(std::getline(system_info, value)){
+      // L1d
+      if(value.find("L1d")!=std::string::npos){
+        L1d=stod(split(value, ' ')[2]);
+      }
+      // L1i
+      if(value.find("L1i")!=std::string::npos){
+        L1i=stod(split(value, ' ')[2]);
+      }
+      // L2
+      if(value.find("L2")!=std::string::npos){
+        L2=stod(split(value, ' ')[2]);
+      }
+      // L3
+      if(value.find("L3")!=std::string::npos){
+        L3=stod(split(value, ' ')[2]);
+      }
+  }
+  std::cout<<"L1d:"<<L1d<<"KB L1i:"<<L1i<<"KB L2:"<<L2<<"MB L3:"<<L3<<"MB"<<std::endl;
+  system_info.close();
+
   for (auto &test : Tests) {
     Vector a(test.N, 1.0);  // 200/300/.../10000000 ones
     Vector b(test.N, 1.0);  // 200/300/.../10000000 ones
     Vector c(test.N, 1.0);  // 200/300/.../10000000 ones
     Vector d(test.N, 1.0);  // 200/300/.../10000000 ones
     for (int n = 0; n != 19; ++n)
-      timeit::run(triad, a, b, c, d); // repetition x19 times of triad(i.e. loop a[i]=b[i]+c[i]*d[i])
+      timeit::run(triad, a, b, c, d); // repeat x19 times of triad(i.e. loop a[i]=b[i]+c[i]*d[i])
     int N = 40;
-    for (int n = 0; n != N; ++n) // iter x40 times of triad and sum up triad results
+    for (int n = 0; n != N; ++n) // averaged triad runtime
       test.runtime += timeit::run(triad, a, b, c, d);
     test.runtime /= N;
   }
