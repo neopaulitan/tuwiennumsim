0d57bb4a513e6fc7d57c421c60697b3673a0131a
Max Spannring <e12104980@tuwien.ac.at>
2024-11-16 16:44:49 +0100

    some aesthetic changes to the code

--- Ex2/task2/main.cpp
+++ Ex2/task2/main.cpp
@@ -1,20 +1,19 @@
 // sudo apt-get install build-essential libeigen3-dev libopenblas-dev
-// g++ main.cpp -std=c++17 -O3 -lopenblas -DEIGEN_DONT_PARALLELIZE -march=native -ffast-math -o mmm
+// g++ main.cpp -std=c++17 -O3 -lopenblas -DEIGEN_DONT_PARALLELIZE -march=native
+// -ffast-math -o mmm
 // ./mmm CUSTOM 1024
 // ./mmm EIGEN 1024
 // export OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 && ./mmm BLAS 1024
 
+#include <cassert>
+#include <cblas.h>
+#include <chrono>
+#include <eigen3/Eigen/Dense>
 #include <iostream>
 #include <stdexcept>
 #include <string>
 #include <tuple>
 #include <vector>
-
-#include <cblas.h>
-#include <eigen3/Eigen/Dense>
-#include <chrono>
-#include <cassert>
-#include <eigen3/Eigen/Dense>
 
 namespace program_options {
 std::tuple<std::string, std::size_t> parse(int argc, char *argv[]) {
@@ -23,86 +22,87 @@ std::tuple<std::string, std::size_t> parse(int argc, char *argv[]) {
   auto mode = std::string(argv[1]);
   const std::vector<std::string> modes = {"CUSTOM", "BLAS", "EIGEN"};
   if (std::find(modes.begin(), modes.end(), mode) == modes.end())
-    throw std::runtime_error("unsupported mode");
+    throw std::runtime_error(
+        "unsupported mode - Choose either 'CUSTOM', 'BLAS', or 'Eigen'");
   std::size_t size;
   if (std::sscanf(argv[2], "%zu", &size) != 1 && size != 0)
     throw std::runtime_error("invalid size");
   std::printf("arguments: impl=%s size=%zu\n", mode.data(), size);
   return {mode, size};
 }
-
 } // namespace program_options
 
-std::chrono::duration<double> MMM_custom(int N){
+std::chrono::duration<double> MMM_custom(int N) {
   // prepare Matricies
   int M[N][N];
   int MT[N][N];
   int MMT[N][N] = {}; // this is the product of M & MT
-  for (int i = 0; i < N; i++){
-    for (int j = 0; j < N; j++){
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
       M[i][j] = i + j * N;
       MT[i][j] = j + i * N;
-    } 
+    }
   }
   // perform actual MMM
   auto start_time = std::chrono::high_resolution_clock::now();
-  for (int i = 0; i < N; i++){
-    for (int j = 0; j < N; j++){
-      for (int k = 0; k < N; k++){
-        MMT[i][j] += M[k][i] * MT[j][k]; 
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      for (int k = 0; k < N; k++) {
+        MMT[i][j] += M[k][i] * MT[j][k];
       }
     }
   }
   auto end_time = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed_time = end_time - start_time;
-  
   // perform symmetry check
-  for (int i = 0; i < N; i++){
-   for (int j = 0; j < N; j++){
-    assert(MMT[i][j] == MMT[j][i]);
-    std::cout << "MMT[" << i << "][ " << j <<"] = " << MMT[i][j] << std::endl;
-     }
-   } 
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      assert(MMT[i][j] == MMT[j][i]);
+      // std::cout << "MMT[" << i << "][ " << j <<"] = " << MMT[i][j] <<
+      // std::endl;
+    }
+  }
   return elapsed_time;
 }
 
-std::chrono::duration<double> MMM_OpenBLAS(int N){
+std::chrono::duration<double> MMM_OpenBLAS(int N) {
   // prepare the matricies
   const double alpha = 1.0;
   const double beta = 0.0;
   std::vector<double> M;
-  for (int i = 0; i < N * N; i++){
+  for (int i = 0; i < N * N; i++) {
     M.push_back(i);
   }
-  std::vector<double> MMT(N*N, 0.0);
-
-// perform the Matrix-Matrix Multiplication
+  std::vector<double> MMT(N * N, 0.0);
+  // perform the Matrix-Matrix Multiplication
   auto start_time = std::chrono::high_resolution_clock::now();
-  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, alpha, M.data(), N, M.data(), N, beta, MMT.data(), N);  
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, alpha, M.data(),
+              N, M.data(), N, beta, MMT.data(), N);
   auto end_time = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed_time = end_time - start_time;
-  
   // perform symmetry check
-  for (int i = 0; i < N; i++){
-    for (int j = 0; j < N; j++){
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
       assert(MMT[i + j * N] == MMT[j + i * N]);
     }
   }
+  /*
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
       std::cout << MMT[i * N + j] << " ";
-      } 
+      }
      std::cout << "\n";
      }
+  */
   return elapsed_time;
 }
 
-std::chrono::duration<double> MMM_Eigen(int N){
+std::chrono::duration<double> MMM_Eigen(int N) {
   // prepare the matricies
   Eigen::MatrixXd M(N, N);
-  for (int i = 0; i < N; i++){
-    for(int j = 0; j < N; j++){
-      M(i, j) = j + i*N;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      M(i, j) = j + i * N;
     }
   }
   Eigen::MatrixXd MT = M.transpose();
@@ -112,36 +112,27 @@ std::chrono::duration<double> MMM_Eigen(int N){
   auto end_time = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed_time = end_time - start_time;
   // perform symmetry check
-  for (int i = 0; i < N; i++){
-   for (int j = 0; j < N; j++){
-    assert(MMT(i, j) == MMT(j, i));
-    std::cout << "MMT(" << i << ", " << j <<") = " << MMT(i,j) << std::endl;
-     }
-   } 
-  
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      assert(MMT(i, j) == MMT(j, i));
+      // std::cout << "MMT(" << i << ", " << j <<") = " << MMT(i,j) <<
+      // std::endl;
+    }
+  }
   return elapsed_time;
 }
-
 
 int main(int argc, char *argv[]) try {
   auto [mode, size] = program_options::parse(argc, argv);
   std::size_t N = size;
-  std::string impl = mode;
-  if (mode == "CUSTOM"){
-      std::cout << "custom mode selected" << std::endl;
-      std::chrono::duration<double> elapsed_time = MMM_custom(N);
-      std::cout <<  elapsed_time.count() << std::endl;
-  } else if (mode == "EIGEN"){
-      std::cout << "Eigen selected" << std::endl;
-      std::chrono::duration<double> elapsed_time = MMM_Eigen(N);
-      std::cout <<  elapsed_time.count() << std::endl;
-   } else if (mode == "BLAS"){
-      std::cout << "blas selected" << std::endl;
-      std::chrono::duration<double> elapsed_time = MMM_OpenBLAS(N);
-      std::cout <<  elapsed_time.count() << std::endl;
-   } else{
-     std::cout << "mode not supported. Use 'CUSTOM', 'EIGEN', or 'BLAS'" << std::endl;
-   }
+  if (mode == "CUSTOM") {
+    std::chrono::duration<double> elapsed_time = MMM_custom(N);
+  } else if (mode == "EIGEN") {
+    std::chrono::duration<double> elapsed_time = MMM_Eigen(N);
+  } else if (mode == "BLAS") {
+    std::chrono::duration<double> elapsed_time = MMM_OpenBLAS(N);
+  }
+  std::cout << elapsed_time.count() << std::endl;
   return EXIT_SUCCESS;
 } catch (std::exception &e) {
   std::cout << e.what() << std::endl;
