699df280566ca8f45618eccb0ce4009f68f7d1f1
NeoPaulitan <paulbeumsukang@gmail.com>
2024-12-05 10:39:35 +0100

    task3completed with code explanation

--- Ex2/task3/plotty.py
+++ Ex2/task3/plotty.py
@@ -1,38 +1,74 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import csv
 
-iter = [10, 100, 1000, 100000, 200000, 500000]
-residtab = np.zeros(6, 3)
+iter = [10, 100, 1000, 10000, 50000]
+itersize = len(iter)
+residtabs = np.zeros((itersize, 3))
 
-for i in range(0,6):
-	file = f"normiter_{iter[i]}.csv"
-	csvfile = csv.reader(file)
-	data = list(reader)
-	residtab[i][0] = data[0]
-	residtab[i][1] = data[1]
-	residtab[i][2] = data[2]
-	file.close()
-	
-#solndata = (128, 2)
-#file = csv.reader("iternum_500000.csv")
-#solndata[:,1] = file 
-#solndata[:,0] = np.arange(0,1,128)
-#file.close()
+for i in range(0,itersize):
+	filename = f"normiter_{iter[i]}.csv"
+	with open(filename, 'r') as file:
+		csvfile = csv.reader(file)
+		data = list(csvfile)
+		residtabs[i][0] = data[0][0]
+		residtabs[i][1] = data[1][0]
+		residtabs[i][2] = data[2][0]
 
-fig1, (ax1,ax2) = plt.subplots(2, figsize = (6,12))
+#fig1, (ax1,ax2) = plt.subplots(2, figsize = (6,12))
+fig1, (ax1,ax2) = plt.subplots(2, 1)
 ax1.scatter(residtabs[:,0], residtabs[:,1])
-ax1.set_xlabel("Number of Iterations")
-#ax1.set_yscale("log")
-ax1.set_ylabel("2-Norm Residuals")
-ax1.set_title("2-Norm Residuals x Number of Iterations for N = 128 and Dirichlet and Neumann boundaries = 0 for a Gaussian point source centered at (0.5,0.5) with a deviation of 0.1")
-
 ax2.scatter(residtabs[:,0], residtabs[:,1])
 ax2.set_xlabel("Number of Iterations")
-ax2.set_ylabel("Inf-Norm Residuals")
-ax2.set_title("Inf-Norm Residuals x Number of Iterations for N = 128 and Dirichlet and Neumann boundaries = 0 for a Gaussian point source centered at (0.5,0.5) with a deviation of 0.1")
+#ax1.set_xscale("log")
+ax1.set_ylabel("2-Norm Residuals")
+ax1.set_title("Plot of 2-Norm by Iterations")
+ax1.set_ylim(0.005, 0.007)
+ax2.set_ylim(-0.0001, 0.0001)
 
-fig.savefig("resplots.png")
+# hide the spines between ax and ax2
+ax1.spines.bottom.set_visible(False)
+ax2.spines.top.set_visible(False)
+ax1.xaxis.tick_top()
+ax1.tick_params(labeltop=False)  # don't put tick labels at the top
+ax2.xaxis.tick_bottom()
+d = .5  # proportion of vertical to horizontal extent of the slanted line
+kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12,
+              linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
+ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+
+fig1.savefig("2normplot.png")
+
+fig2, (ax1,ax2) = plt.subplots(2, 1)
+ax1.scatter(residtabs[:,0], residtabs[:,2])
+ax2.scatter(residtabs[:,0], residtabs[:,2])
+ax2.set_xlabel("Number of Iterations")
+ax2.set_ylabel("Inf-Norm Residuals")
+ax1.set_title("Plot of Inf-Norm by Iterations")
+ax1.scatter(residtabs[:,0], residtabs[:,1])
+ax2.scatter(residtabs[:,0], residtabs[:,1])
+ax2.set_xlabel("Number of Iterations")
+#ax1.set_xscale("log")
+ax1.set_ylabel("Inf-Norm Residuals")
+ax1.set_ylim(0.005, 0.007)
+ax2.set_ylim(-0.0001, 0.0001)
+
+# hide the spines between ax and ax2
+ax1.spines.bottom.set_visible(False)
+ax2.spines.top.set_visible(False)
+ax1.xaxis.tick_top()
+ax1.tick_params(labeltop=False)  # don't put tick labels at the top
+ax2.xaxis.tick_bottom()
+d = .5  # proportion of vertical to horizontal extent of the slanted line
+kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12,
+              linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
+ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+
+
+fig2.savefig("infnormplot.png")
 
 def plot_and_save(filename):
     arr = np.loadtxt(filename, delimiter=" ", dtype=float)
@@ -43,10 +79,7 @@ def plot_and_save(filename):
     fig.savefig(filename + ".png")
     plt.show()
 
-plot_and_save("iternum_500000.csv")
+plot_and_save("iternum_100.csv")
+plot_and_save("iternum_50000.csv")
 
-#ax3.scatter(solndata[:,0],solndata[:,1])
-#ax3.set_xlabel("x")
-#ax3.set_ylabel("u(x,y)")
-#ax3.set_title("Generated Solution for Poisson Problem using Jacobi Solver after 500000 iterations")
 plt.show()
