327a13d526015d7cfab9d733fa911c69493755c9
Bailiang Li <lbllbl93@gmail.com>
2024-12-23 01:39:38 +0100

    better plot

--- Ex3/task1/plot.py
+++ Ex3/task1/plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def read_data(filename):
     x = []
@@ -13,11 +14,19 @@ def read_data(filename):
 
 x1, y1 = read_data('res_eor.txt')
 x2, y2 = read_data('Anorm_eor.txt')
+# convergence tendency
+coef1 = np.polyfit(np.log10(x1), np.log10(y1), 1)
+line1 = np.poly1d(coef1)
+coef2 = np.polyfit(np.log10(x2), np.log10(y2), 1)
+line2 = np.poly1d(coef2)
 
-plt.figure(figsize=(8, 6))
+plt.figure(figsize=(16, 12))
 
-plt.plot(x1, y1, 'o-', color='blue', linewidth=0.5, label='res_eor_data')
+plt.plot(x1, y1, 'o', color='blue', linewidth=0.5, label='res_eor_data')
 plt.plot(x2, y2, 'o-', color='red', linewidth=0.5, label='Anorm_eor_data')
+x_range = np.linspace(min(x1), max(x1), 500)
+plt.plot(x_range, 10**line1(np.log10(x_range)), 'blue', linestyle='--', label='_nolegend_')
+# plt.plot(x_range, 10**line2(np.log10(x_range)), 'blue', linestyle='--', label='Trendline 2')
 for i in range(len(x1)):
     plt.text(x1[i], y1[i]*2, f'{y1[i]:.2e}', fontsize=5, color='blue', ha='center')
 for i in range(len(x2)):
